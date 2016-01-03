54ca8c59f73af90af0f66210df6072c5fc6e6b1c
Mikail Tolga Turan <Turan1102@hotmail.com>
2016-01-03 20:27:39 +0100

    update references test

--- FerrySystem/Test/InterfaceStub/CustomerInterfaceStub.cs
+++ FerrySystem/Test/InterfaceStub/CustomerInterfaceStub.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contract.contract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace Test.InterfaceStub
 {
-    class CustomerInterfaceStub
+    class CustomerInterfaceStub : CustomerContract
     {
+        public List<Contract.dto.Trip> GetAllTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contract.dto.Reservation> GetAllCustomerReservations(Contract.dto.Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateCustomer(Contract.dto.Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contract.dto.Customer GetCustomerByLogin(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contract.dto.Reservation CreateCustomerReservation(Contract.dto.Trip trip, Contract.dto.Customer customer, double totalPrice, int numberOfPeople, Contract.dto.Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelCustomerReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contract.dto.Route> GetAllRoutes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contract.dto.Vehicle> GetAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
